3fd0822264ec6c920315f6bf7069f51d187ec8c8
illmet <ilyaamet@gmail.com>
2024-07-26 10:24:29 +0100

    Cleanup

--- data.py
+++
@@ -1,9 +0,0 @@
-#from os.path import join
-
-#from dataset import DatasetFromFolder_Test
-
-
-#def get_test_set(root_dir):
-    #test_dir = join(root_dir)
-
-    #return DatasetFromFolder_Test(test_dir)
